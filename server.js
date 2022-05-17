36ead5db588a3fe99903620dc8c64e963cfb6002
Raquel Olaya <raquelolaya@Raquels-MacBook-Air.local>
2022-05-17 01:01:55 -0500

    name done

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-const express = require("express");
+const express = require("express")
 const app = express();
 
 const port = 3000;
-app.get('/', (req, res) {
+
+app.get('/', (req, res) => {
     console.log("Oh hey! I got a request. Let me respond with ssomething");
-    req.send("Hello")
-})
+    req.send("Hello");
+});
+
 app.get("/greeting/:name", (req, res) => {
     console.log(req.params);
-    res.send("Whats up" + req.params.name);
+    res.send("Whats up" + " " + req.params.name);
 });
 
 app.listen(3000, () => {
     console.log("Express is listening for request from the browser")
 });
-
 
