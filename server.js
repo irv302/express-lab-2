96a3f2c1b051f34fdcc48691825f4e824cbf1828
Raquel Olaya <raquelolaya@Raquels-MacBook-Air.local>
2022-05-17 00:36:44 -0500

    express-lab

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+app.get('/', (req, res) {
+    console.log("Oh hey! I got a request. Let me respond with ssomething");
+    req.send("Hello")
+})
+app.get("/greeting/:name", (req, res) => {
+    console.log(req.params);
+    res.send("Whats up" + req.params.name);
+});
+
+app.listen(3000, () => {
+    console.log("Express is listening for request from the browser");
+});
