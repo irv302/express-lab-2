ecc17b72d08ff6d8182b4da7d1122860c7fb1114
Raquel Olaya <raquelolaya@Raquels-MacBook-Air.local>
2022-05-17 02:30:06 -0500

    calculator

--- server.js
+++ server.js
@@ -3,17 +3,32 @@ const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-    console.log("Oh hey! I got a request. Let me respond with ssomething");
-    req.send("Hello");
+// app.get('/', (req, res) => {
+//     console.log("Oh hey! I got a request. Let me respond with ssomething");
+//     req.send("Hello");
+// });
+// //Greetings
+// app.get("/greeting/:name", (req, res) => {
+//     console.log(req.params);
+//     res.send("Whats up" + " " + req.params.name);
+// });
+
+// app.listen(3000, () => {
+//     console.log("Express is listening for request from the browser")
+// });
+
+
+//tip calculator
+
+
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+    console.log(req.params);
+    res.send(req.params.tipPercentage);
 });
 
-app.get("/greeting/:name", (req, res) => {
-    console.log(req.params);
-    res.send("Whats up" + " " + req.params.name);
-});
 
 app.listen(3000, () => {
     console.log("Express is listening for request from the browser")
 });
 
+//
